31368d28c2a8809155486f15b33d01e6cf540c08
martin_brandstetter <martin.brandstetter@apro.at>
2020-05-15 11:47:53 +0200

    modified camera preview implemenation for taking photos without preview

--- android/src/main/java/com/google/android/cameraview/CameraView.java
+++ android/src/main/java/com/google/android/cameraview/CameraView.java
@@ -160,11 +160,9 @@ public class CameraView extends FrameLayout {
     @NonNull
     private PreviewImpl createPreviewImpl(Context context) {
         PreviewImpl preview;
-        if (Build.VERSION.SDK_INT < 14) {
-            preview = new SurfaceViewPreview(context, this);
-        } else {
-            preview = new TextureViewPreview(context, this);
-        }
+        
+        preview = new SurfaceViewPreview(context, this);
+
         return preview;
     }
 
